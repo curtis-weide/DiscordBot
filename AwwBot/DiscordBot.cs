b3d065fc3a06d3dcd53fc0b5b4eeec6740f25997
Curtis Weide <Curtis Weide>
2017-04-21 19:52:14 -0500

    Add project files.

---
+++ AwwBot/DiscordBot.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+
+namespace AwwBot
+{
+    public class DiscordBot
+    {
+        DiscordClient client;
+        CommandService commands;
+
+        public DiscordBot()
+        {
+            
+
+            client = new DiscordClient(input =>
+            {
+                input.LogLevel = LogSeverity.Info;
+                input.LogHandler = Log;
+            });
+
+            client.UsingCommands(input =>
+            {
+                input.PrefixChar = Convert.ToChar("!");
+                input.AllowMentionPrefix = true;
+            });
+
+            commands = client.GetService<CommandService>();
+
+            commands.CreateCommand("help").Do(async (e) =>
+            {
+                await e.Channel.SendMessage("For a r/aww picture, type !aww. For a r/rarepuppers picture, type !pupper. For a picture of Luna, type !Luna");
+            });
+
+            client.ExecuteAndWait(async () =>
+            {
+                await client.Connect("MzA1MTMxOTkzMDc3OTA3NDY3.C9wv1A.PM_jAGFN7DwM2r8Ln3mmw05q4Ak", TokenType.Bot);
+            });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
